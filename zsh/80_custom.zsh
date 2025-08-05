a97b72f258e301bb72c412520fb89ce59ff12d28
akgm3i <51406499+akgm3i@users.noreply.github.com>
2025-08-05 18:30:54 +0900

    zsh: update aliases to use exa for improved file listing and add settings for loading asdf

--- zsh/80_custom.zsh
+++ zsh/80_custom.zsh
@@ -1,1 +1,16 @@
 # sheldon completions --shell zsh > /path/to/completions/_sheldon
+
+# asdf
+export PATH="${ASDF_DATA_DIR}/shims:$PATH"
+# append completions to fpath
+fpath=(${ASDF_DATA_DIR}/completions $fpath)
+# initialise completions with ZSH's compinit
+autoload -Uz compinit && compinit -d "${XDG_CACHE_HOME}/zsh/zcompdump-${ZSH_VERSION}"
+
+
+function cdrepo {
+  local repodir="$( ghq list -p | fzf -1 +m )"
+  if [ ! -z "$repodir" ] ; then
+    cd "$repodir"
+  fi
+}
