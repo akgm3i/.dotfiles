b3132b7ea0a90ef26a9e6228a58c714112417f21
akgm3i <51406499+akgm3i@users.noreply.github.com>
2025-08-05 18:29:10 +0900

    zsh: update plugin configurations for improved loading and organization

--- zsh/zinit_load.zsh
+++ zsh/zinit_load.zsh
@@ -7,35 +7,35 @@
 
 
 # 🌸 A command-line fuzzy finder
-zinit ice as'program' from'gh-r' pick'fzf'
+zinit ice as'program' from'gh-r'
 zinit light junegunn/fzf-bin
 
 # Remote repository management made easy
-zinit ice as'program' from'gh-r' pick'**/ghq'
+zinit ice as'program' from'gh-r' pick'ghq**/ghq'
 zinit light x-motemen/ghq
 
 # a lightweight and flexible command-line JSON processor.
-zinit ice as'program' from'gh-r' pick'jq* -> jq'
+zinit ice as'program' from'gh-r' mv'jq* -> jq'
 zinit light jqlang/jq
 
 # # A command-line tool that makes git easier to use with GitHub.
 # zinit ice from'gh-r' sbin'**/hub' cp'**/hub.zsh_completion -> _hub' atload'eval "$(hub alias -s)"'
 # zinit light github/hub
 
-# # Extendable version manager
-# zinit ice src'asdf.sh' atclone'git checkout "$(git describe --abbrev=0 --tags)"' atpull'%atclone'
-# zinit light asdf-vm/asdf
+# Extendable version manager
+zinit ice as'program' from'gh-r'
+zinit light asdf-vm/asdf
 
 # a simple, fast and user-friendly alternative to find.
-zinit ice as'program' from'gh-r' pick'**/fd'
+zinit ice as'program' from'gh-r' pick'fd/fd'
 zinit light sharkdp/fd
 
 # syntax highlighting for a large number of programming and markup languages
-zinit ice as'program' from'gh-r' pick'**/bat'
+zinit ice as'program' from'gh-r' pick'bat**/bat'
 zinit light sharkdp/bat
 
 # a replacement for ls written in Rust.
-zinit ice as'program' from'gh-r' pick'exa* -> exa'
+zinit ice from'gh-r' as'program' pick'bin/exa'
 zinit light ogham/exa
 
 # docker completion
