e539c277fdfcad9664fe40bd61858061b23264a4
akgm3i <51406499+akgm3i@users.noreply.github.com>
2025-08-06 00:12:06 +0900

    zsh: update aliases exa to eza

--- zsh/30_aliases.zsh
+++ zsh/30_aliases.zsh
@@ -4,13 +4,13 @@
 
 # Common aliases
 alias ..='cd ../'
-alias ld='exa -ld'          # Show info about the directory
-alias ll='exa -lF'          # Show long file information
-alias la='exa -aF'          # Show hidden files
-alias lla='exa -laF'        # Show hidden all files
-alias lx='exa -lXB'         # Sort by extension
-alias lk='exa -lSr'         # Sort by size, biggest last
-alias lt='exa -ltr'         # Sort by date, most recent last
-alias lc='exa -ltcr'        # Sort by and show change time, most recent last
-alias lu='exa -ltur'        # Sort by and show access time, most recent last
-alias lr='exa -lR'          # Recursive ls
+alias ld='eza -ld'          # Show info about the directory
+alias ll='eza -lF'          # Show long file information
+alias la='eza -aF'          # Show hidden files
+alias lla='eza -laF'        # Show hidden all files
+alias lx='eza -lXB'         # Sort by extension
+alias lk='eza -lSr'         # Sort by size, biggest last
+alias lt='eza -ltr'         # Sort by date, most recent last
+alias lc='eza -ltcr'        # Sort by and show change time, most recent last
+alias lu='eza -ltur'        # Sort by and show access time, most recent last
+alias lr='eza -lR'          # Recursive ls
